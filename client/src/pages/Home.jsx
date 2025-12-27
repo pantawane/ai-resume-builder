411c27040734ddceb56b3731f1a53ed4eb90be0b
Student ApnaCollege <pantawanevansh75@gmail.com>
2025-12-27 08:48:20 +0530

    project completed

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Banner from '../components/home/Banner'
+import Hero from '../components/home/Hero'
+import Features from '../components/home/Features'
+import Testimonisl from '../components/home/Testimonisl'
+import CallToAction from '../components/home/CallToAction'
+import Footer from '../components/home/Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Hero />
+      <Features />
+      <Testimonisl />
+      <CallToAction />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
