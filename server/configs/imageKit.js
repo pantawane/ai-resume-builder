e2b16dc9caa6bdc47633b578d52cd12ab4f20845
Student ApnaCollege <pantawanevansh75@gmail.com>
2026-01-27 15:12:18 +0530

    Fix imagekit filename case

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,4 +1,4 @@
-import ImageKit from '@imageKit/nodejs';
+import ImageKit from '@imagekit/nodejs';
 
 const imagekit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,4 +1,4 @@
-import imagekit from "../configs/imageKit.js";
+import imagekit from "../configs/imagekit.js";
 import Resume from "../models/Resume.js";
 import fs from "fs";
 
